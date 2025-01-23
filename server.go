2567145c4945e9c36d5111ad6f86ef5ab93b67fc
Алексей Зендель <alexeizendel@yandex.ru>
2025-01-23 14:50:24 +0500

    Звуки сохраняются и записываются в файл. (Требуется рефакторинг)

--- server.go
+++ server.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 type Message struct {
@@ -30,6 +31,9 @@ func handleFuncHome(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+const SOUNDS_PATH_PREFIX = ""
+const SOUNDS_NAMES_PATH = "sounds.txt"
+
 func handleFuncUploadSound(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost:
@@ -37,8 +41,19 @@ func handleFuncUploadSound(w http.ResponseWriter, r *http.Request) {
 		json.NewDecoder(r.Body).Decode(&msg)
 		data, err := base64.StdEncoding.DecodeString(msg.Text)
 		logPotentialErr(err)
-		err = os.WriteFile(msg.Name, data, 0644)
+		err = os.WriteFile(SOUNDS_PATH_PREFIX+msg.Name, data, 0644)
 		logPotentialErr(err)
+
+		f, err := os.OpenFile(SOUNDS_NAMES_PATH, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if _, err := f.Write([]byte(strings.Split(msg.Name, ".")[0] + " " + msg.Name + "\n")); err != nil {
+			log.Fatal(err)
+		}
+		if err := f.Close(); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
